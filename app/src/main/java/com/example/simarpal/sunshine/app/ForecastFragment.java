a807f27601f03280c5a395faaede0671251a195a
simar1998 <simarpal.kalsi@gmail.com>
2015-09-18 01:17:18 -0400

    JSON
    Succesfully get JSON string from openweathermap server.
    I will add the sorting of the JSON string for desirable string values
    soon. I am sleppy.

--- app/src/main/java/com/example/simarpal/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/simarpal/sunshine/app/ForecastFragment.java
@@ -1,10 +1,14 @@
 package com.example.simarpal.sunshine.app;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -24,14 +28,36 @@ import static com.example.simarpal.sunshine.app.R.layout.list_item_forecast;
  * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
-
+    ListView listView;
     public ForecastFragment() {
     }
-
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh)
+        {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         String[] weatherArray = {"Today-sunny-88/63","Tomorrow-sunny-88/63","Monday-cloudy-88/63","Tuesday-sunny-88/63"
                 ,"Wednesday-rsin-88/63","Friday-hail-88/63"};
         ArrayList<String> weatherData = new ArrayList<String>();
@@ -42,16 +68,8 @@ public class ForecastFragment extends Fragment {
         weatherData.add("Wednesday-rsin-88/63");
         weatherData.add("Friday-hail-88/63");
         ArrayAdapter mForecastAdapter = new ArrayAdapter<String>(getActivity(), list_item_forecast, R.id.list_item_forecast_textview,weatherData);
-        ListView listView = (ListView) getActivity().findViewById(R.id.list_item_forecast);
+        listView = (ListView) getView().findViewById(R.id.list_item_forecast);
         listView.setAdapter(mForecastAdapter);
-
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-
     }
 }
 class FetchWeatherTask extends AsyncTask<Void,Void,Void>
@@ -66,11 +84,23 @@ private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
             // Construct the URL for the OpenWeatherMap query
             // Possible parameters are avaiable at OWM's forecast API page, at
             // http://openweathermap.org/API#forecast
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
 
             // Create the request to OpenWeatherMap, and open the connection
+
+            final String QUERY_PARAM = "q";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+
+            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM,"Toronto")
+                    .appendQueryParameter(FORMAT_PARAM,"json")
+                    .appendQueryParameter(UNITS_PARAM,"metric")
+                    .appendQueryParameter(DAYS_PARAM,"7").build();
+            URL url = new URL(builtUri.toString());
+
             urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
             // Read the input stream into a String
@@ -95,6 +125,7 @@ private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
                 return null;
             }
             forecastJsonStr = buffer.toString();
+            Log.v(LOG_TAG, " Forecast JSON String : "+ forecastJsonStr);
         } catch (IOException e) {
             Log.e("PlaceholderFragment", "Error ", e);
             // If the code didn't successfully get the weather data, there's no point in attemping
